bd22aa1f9e1d2e1781380e9c7102821d3adb17ef
daadaadaah <daadaadaah@gmail.com>
2023-01-02 22:58:37 +0900

    스프링 빈을 등록하는 방법2. 자바 코드로 직접 스프링 빈 등록하기

--- kim-young-han/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ kim-young-han/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 //    // 방법 1
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
