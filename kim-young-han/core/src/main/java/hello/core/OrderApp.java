5dedd452d5d5b84548d937f07c9b6d70d8f42242
daadaadaah <daadaadaah@gmail.com>
2023-01-03 19:05:10 +0900

    [스프링 핵심 월리 이해2-객체 지향 원리 적용] AppConfig로 관심사 분리함으로서 DIP 위반 문제 해결 - `AppConfig`는 구체 클래스를 선택한다. 애플리케이션이 어떻게 동작해야 할지 전체 구성을 책임진다. - `OrderServiceImpl`는 기능을 실행하는 책임만 지면 된다.

--- kim-young-han/core/src/main/java/hello/core/OrderApp.java
+++ kim-young-han/core/src/main/java/hello/core/OrderApp.java
@@ -11,8 +11,9 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) { // 자동 완성 : psvm
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
--- kim-young-han/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ kim-young-han/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,6 +1,8 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +10,13 @@ import static org.assertj.core.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
