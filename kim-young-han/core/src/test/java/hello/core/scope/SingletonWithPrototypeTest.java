9d0be668d1b153fc1478766384f1a2f664dd3a33
daadaadaah <daadaadaah@gmail.com>
2023-01-05 20:49:16 +0900

    [빈 스코프] 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 발생하는 문제 : javax.inject.Provider 라는 JSR-330 자바 표준로 문제 해결

--- kim-young-han/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ kim-young-han/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,10 +31,10 @@ public class SingletonWithPrototypeTest {
     static class ClientBean {
 
         @Autowired
-        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
