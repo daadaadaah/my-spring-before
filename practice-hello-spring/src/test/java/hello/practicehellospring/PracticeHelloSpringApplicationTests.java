144aa092bf404aa629cddeef5bdf9d3a2f6c889c
daadaadaah <daadaadaah@gmail.com>
2023-03-10 17:55:23 +0900

    프로젝트 생성

---
+++ practice-hello-spring/src/test/java/hello/practicehellospring/PracticeHelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.practicehellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeHelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
